0a39adc8296c6fce131f88cfb6ac329a9faafe85
Saw Khaing <sawkhaing.mm@gmail.com>
2022-11-13 20:07:05 +0700

    update

--- providers.tf
+++ providers.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate3770"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
